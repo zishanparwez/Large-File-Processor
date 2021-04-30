0b23e3421adaf9f3a3286b423ea59520034d6e7a
zishanparwez <32333911+zishanparwez@users.noreply.github.com>
2021-04-30 18:49:38 +0530

    Update index.js

--- api/config/index.js
+++ api/config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-    DB: 'mongodb+srv://zishan:1999@Aslam@cluster0.f3lp9.mongodb.net/large-file-processor?retryWrites=true&w=majority',
+    DB: 'mongodb+srv://large-file-processor:postmanuser@cluster0.f3lp9.mongodb.net/large-file-processor?retryWrites=true&w=majority',
     PORT: 3000
   };
